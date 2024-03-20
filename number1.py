6caaf3d52e8d257920522f340dd7d8f5889df848
kurtkabeinaa <163307903+kurtkabeinaa@users.noreply.github.com>
2024-03-20 16:54:16 +0300

    Update number1.py

--- number1.py
+++ number1.py
@@ -11,7 +11,7 @@ with open ('game.txt', encoding='utf-8') as file:
     answer = reader[1:]
 for el in answer:
     if '55' in el[2]:
-        print(f'У персонажа {el[0]} в игре {el[1]} нашлась ошибка с кодом: {el[2]}. Дата фиксации: {el[3]}')
+        print(f'У персонажа {el[1]} в игре {el[0]} нашлась ошибка с кодом: {el[2]}. Дата фиксации: {el[3]}')
         el[2] = 'Done'
         el[3] = '0000-00-00'
 with open ('game_new.csv', 'w', newline='', encoding='utf-8') as newfile:
